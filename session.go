3b0e8e402fc0b3baec4583a2f693edcc3cfaa448
tuxranger <11413392+tuxranger@users.noreply.github.com>
2020-11-12 08:53:22 -0500

    Impulse#105 jaeger (#3)
    * motiv-labs/impulse#105 added tracing
    
    * motiv-labs/impulse#105 reorder query params

--- session.go
+++ session.go
@@ -1,28 +1,31 @@
 package cassandra
 
-import "github.com/gocql/gocql"
+import (
+	"github.com/gocql/gocql"
+	"github.com/opentracing/opentracing-go"
+)
 
 // Initializer is a common interface for functionality to start a new session
 type Initializer interface {
-	NewSession() (Holder, error)
+	NewSession(parentSpan opentracing.Span) (Holder, error)
 }
 
 // Holder allows to store a close sessions
 type Holder interface {
-	GetSession() SessionInterface
-	CloseSession()
+	GetSession(parentSpan opentracing.Span) SessionInterface
+	CloseSession(parentSpan opentracing.Span)
 }
 
 // SessionInterface is an interface to wrap gocql methods used in Motiv
 type SessionInterface interface {
-	Query(stmt string, values ...interface{}) QueryInterface
-	Close()
+	Query(parentSpan opentracing.Span, stmt string, values ...interface{}) QueryInterface
+	Close(parentSpan opentracing.Span)
 }
 
 type QueryInterface interface {
-	Exec() error
-	Scan(dest ...interface{}) error
-	Iter() *gocql.Iter
-	PageState(state []byte) *gocql.Query
-	PageSize(n int) *gocql.Query
+	Exec(parentSpan opentracing.Span) error
+	Scan(parentSpan opentracing.Span, dest ...interface{}) error
+	Iter(parentSpan opentracing.Span) *gocql.Iter
+	PageState(state []byte, parentSpan opentracing.Span) *gocql.Query
+	PageSize(n int, parentSpan opentracing.Span) *gocql.Query
 }
--- sessionretry.go
+++ sessionretry.go
@@ -3,6 +3,7 @@ package cassandra
 import (
 	"github.com/gocql/gocql"
 	log "github.com/motiv-labs/logwrapper"
+	"github.com/opentracing/opentracing-go"
 )
 
 // sessionRetry is an implementation of SessionInterface
@@ -11,14 +12,24 @@ type sessionRetry struct {
 }
 
 // Query wrapper to be able to return our own QueryInterface
-func (s sessionRetry) Query(stmt string, values ...interface{}) QueryInterface {
+func (s sessionRetry) Query(parentSpan opentracing.Span, stmt string, values ...interface{}) QueryInterface {
+	span := opentracing.StartSpan("Query", opentracing.ChildOf(parentSpan.Context()))
+	defer span.Finish()
+	span.SetTag("Module", "cassandra")
+	span.SetTag("Interface", "sessionRetry")
+
 	log.Debug("running SessionRetry Query() method")
 
 	return queryRetry{goCqlQuery: s.goCqlSession.Query(stmt, values...)}
 }
 
 // Close wrapper to be able to run goCql method
-func (s sessionRetry) Close() {
+func (s sessionRetry) Close(parentSpan opentracing.Span) {
+	span := opentracing.StartSpan("Close", opentracing.ChildOf(parentSpan.Context()))
+	defer span.Finish()
+	span.SetTag("Module", "cassandra")
+	span.SetTag("Interface", "sessionRetry")
+
 	log.Debug("running SessionRetry Close() method")
 
 	s.goCqlSession.Close()
