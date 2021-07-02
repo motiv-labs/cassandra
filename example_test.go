8b65c6db9fa440fa84c1737d8a6c00d2690386c6
Aristotle <11413392+tuxranger@users.noreply.github.com>
2021-07-02 11:38:21 -0400

    motiv-labs/impulse#205 setting default system organization and env var

--- example_test.go
+++ example_test.go
@@ -1,11 +1,22 @@
 package cassandra
 
 import (
+	"context"
+	impulse_ctx "github.com/motiv-labs/impulse-ctx"
 	jaegermod "github.com/motiv-labs/jaeger"
 	log "github.com/motiv-labs/logwrapper"
 	"github.com/opentracing/opentracing-go"
 	"time"
 )
+
+func NewMockCtx() context.Context {
+	span := getSpan()
+	return context.WithValue(context.Background(), impulse_ctx.ImpulseCtxKey, impulse_ctx.ImpulseCtx{
+		Organization: "",
+		Span:         span,
+		TraceId:      "",
+	})
+}
 
 // getSpan simply retuns a real span to show passed in the Kafka examples.
 func getSpan() opentracing.Span {
@@ -15,51 +26,53 @@ func getSpan() opentracing.Span {
 }
 
 func Example() {
-	span := getSpan()
+	ctx := NewMockCtx()
+	impulseCtx := ctx.Value(impulse_ctx.ImpulseCtxKey).(impulse_ctx.ImpulseCtx)
 
 	// Cassandra initialization - initializes Cassandra keyspace and creates tables if required
 	// Needs to be called only on the app startup
-	Initialize("system_keyspace", "application_keyspace", 120*time.Second, span)
+	Initialize("system_keyspace", "application_keyspace", 120*time.Second, ctx)
 
 	// Now that Cassandra is initialized we can start new connections
 
 	// Getting a cassandra connection initializer
-	sessionInitializer := New("application_keyspace", span)
+	sessionInitializer := New("application_keyspace", ctx)
 
 	// Starting a new cassandra session
-	sessionHolder, err := sessionInitializer.NewSession(span)
+	sessionHolder, err := sessionInitializer.NewSession(ctx)
 	if err != nil {
-		log.Errorf("error initializing cassandra session - %v", err)
+		log.Errorf(impulseCtx, "error initializing cassandra session - %v", err)
 		return
 	}
 
-	defer sessionHolder.CloseSession(span)
+	defer sessionHolder.CloseSession(ctx)
 
 	// Getting the cassandra session
-	session := sessionHolder.GetSession(span)
+	session := sessionHolder.GetSession(ctx)
 	// And have fun with the session, example:
-	session.Query(span, "SELECT * FROM my_table")
+	session.Query(ctx, "SELECT * FROM my_table")
 }
 
 func ExampleNew() {
-	span := getSpan()
+	ctx := NewMockCtx()
+	impulseCtx := ctx.Value(impulse_ctx.ImpulseCtxKey).(impulse_ctx.ImpulseCtx)
 
 	// Getting a cassandra connection initializer
-	sessionInitializer := New("application_keyspace", span)
+	sessionInitializer := New("application_keyspace", ctx)
 
 	// Starting a new cassandra session
-	sessionHolder, err := sessionInitializer.NewSession(span)
+	sessionHolder, err := sessionInitializer.NewSession(ctx)
 	if err != nil {
-		log.Errorf("error initializing cassandra session - %v", err)
+		log.Errorf(impulseCtx, "error initializing cassandra session - %v", err)
 		return
 	}
 
-	defer sessionHolder.CloseSession(span)
+	defer sessionHolder.CloseSession(ctx)
 
 	// Getting the cassandra session
-	session := sessionHolder.GetSession(span)
+	session := sessionHolder.GetSession(ctx)
 	// And have fun with the session, example:
-	session.Query(span, "SELECT * FROM my_table").Exec(span)
+	session.Query(ctx, "SELECT * FROM my_table").Exec(ctx)
 }
 
 func Example_iterRetry_Scan() {
@@ -71,35 +84,36 @@ func Example_iterRetry_Scan() {
 		Data4 string
 	}
 
-	span := getSpan()
+	ctx := NewMockCtx()
+	impulseCtx := ctx.Value(impulse_ctx.ImpulseCtxKey).(impulse_ctx.ImpulseCtx)
 
 	// Getting a cassandra connection initializer
-	sessionInitializer := New( "application_keyspace", span)
+	sessionInitializer := New( "application_keyspace", ctx)
 
 	// Starting a new cassandra session
-	sessionHolder, err := sessionInitializer.NewSession(span)
+	sessionHolder, err := sessionInitializer.NewSession(ctx)
 	if err != nil {
-		log.Errorf("error initializing cassandra session - %v", err)
+		log.Errorf(impulseCtx, "error initializing cassandra session - %v", err)
 		return
 	}
 
-	defer sessionHolder.CloseSession(span)
+	defer sessionHolder.CloseSession(ctx)
 
 	// Getting the cassandra session
-	session := sessionHolder.GetSession(span)
+	session := sessionHolder.GetSession(ctx)
 
 	// And have fun with the session, example:
-	iter := session.Query(span, "SELECT * FROM my_table").Iter(span)
+	iter := session.Query(ctx, "SELECT * FROM my_table").Iter(ctx)
 
 	var data Data
 	var dataList []Data
 
-	for iter.Scan(span, &data.Data1, &data.Data2, &data.Data3, &data.Data4) {
+	for iter.Scan(ctx, &data.Data1, &data.Data2, &data.Data3, &data.Data4) {
 		dataList = append(dataList, data)
 	}
 
-	if err := iter.Close(span); err != nil {
-		log.Error("error while querying table")
+	if err := iter.Close(ctx); err != nil {
+		log.Error(impulseCtx, "error while querying table")
 		// return ...
 	}
 
@@ -115,34 +129,35 @@ func Example_iterRetry_ScanAndClose() {
 		Data4 string
 	}
 
-	span := getSpan()
+	ctx := NewMockCtx()
+	impulseCtx := ctx.Value(impulse_ctx.ImpulseCtxKey).(impulse_ctx.ImpulseCtx)
 
 	// Getting a cassandra connection initializer
-	sessionInitializer := New("application_keyspace", span)
+	sessionInitializer := New("application_keyspace", ctx)
 
 	// Starting a new cassandra session
-	sessionHolder, err := sessionInitializer.NewSession(span)
+	sessionHolder, err := sessionInitializer.NewSession(ctx)
 	if err != nil {
-		log.Errorf("error initializing cassandra session - %v", err)
+		log.Errorf(impulseCtx,"error initializing cassandra session - %v", err)
 		return
 	}
 
-	defer sessionHolder.CloseSession(span)
+	defer sessionHolder.CloseSession(ctx)
 
 	// Getting the cassandra session
-	session := sessionHolder.GetSession(span)
+	session := sessionHolder.GetSession(ctx)
 
 	// And have fun with the session, example:
-	iter := session.Query(span, "SELECT * FROM my_table").Iter(span)
+	iter := session.Query(ctx, "SELECT * FROM my_table").Iter(ctx)
 
 	var data Data
 	var dataList []Data
 
-	if err := iter.ScanAndClose(span, func() bool {
+	if err := iter.ScanAndClose(ctx, func() bool {
 		dataList = append(dataList, data)
 		return true
 	}, &data.Data1, &data.Data2, &data.Data3, &data.Data4); err != nil {
-		log.Error("error while querying table")
+		log.Error(impulseCtx,"error while querying table")
 		// return ...
 	}
 
--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.14
 
 require (
 	github.com/gocql/gocql v0.0.0-20200624222514-34081eda590e
+	github.com/motiv-labs/impulse-ctx v0.0.0-20210623155859-24f4bdfbd0c9
 	github.com/motiv-labs/jaeger v0.0.0-20210623183733-3c1f20258e68
-	github.com/motiv-labs/logwrapper v0.0.0-20210623183622-440cf44936b0
+	github.com/motiv-labs/logwrapper v0.0.0-20210702153104-13e7851a99e8
 	github.com/opentracing/opentracing-go v1.2.0
 )
