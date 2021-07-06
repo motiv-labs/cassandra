6c8fa68f293c9c607bc151b7688eda910eb96536
tuxranger <11413392+tuxranger@users.noreply.github.com>
2021-07-06 13:16:48 -0400

    Impulse#205 multitennantify (#8)
    * motiv-labs/impulse#205 updated go mod. added context to funcs
    
    * updated go mod
    
    * motiv-labs/impulse#205 setting default system organization and env var
    
    * updated go mods

--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.14
 
 require (
 	github.com/gocql/gocql v0.0.0-20200624222514-34081eda590e
-	github.com/motiv-labs/jaeger v0.0.0-20210107193704-a9b0efa6157e
-	github.com/motiv-labs/logwrapper v0.0.0-20210107191541-8fc08c714551
+	github.com/motiv-labs/impulse-ctx v0.0.0-20210706170111-4ae4e6fb3128
+	github.com/motiv-labs/jaeger v0.0.0-20210706171217-71514d0d640a
+	github.com/motiv-labs/logwrapper v0.0.0-20210706170058-11a389433a15
 	github.com/opentracing/opentracing-go v1.2.0
 )
--- sessionretry.go
+++ sessionretry.go
@@ -1,7 +1,9 @@
 package cassandra
 
 import (
+	"context"
 	"github.com/gocql/gocql"
+	impulse_ctx "github.com/motiv-labs/impulse-ctx"
 	log "github.com/motiv-labs/logwrapper"
 	"github.com/opentracing/opentracing-go"
 )
@@ -12,25 +14,51 @@ type sessionRetry struct {
 }
 
 // Query wrapper to be able to return our own QueryInterface
-func (s sessionRetry) Query(parentSpan opentracing.Span, stmt string, values ...interface{}) QueryInterface {
-	span := opentracing.StartSpan("Query", opentracing.ChildOf(parentSpan.Context()))
-	defer span.Finish()
-	span.SetTag("Module", "cassandra")
-	span.SetTag("Interface", "sessionRetry")
+func (s sessionRetry) Query(ctx context.Context, stmt string, values ...interface{}) QueryInterface {
+	var span opentracing.Span
+	impulseCtx, ok := ctx.Value(impulse_ctx.ImpulseCtxKey).(impulse_ctx.ImpulseCtx)
+	if !ok {
+		log.Warnf(impulseCtx, "ImpulseCtx isn't correct type")
+		span = opentracing.StartSpan("Query")
+		defer span.Finish()
+		span.SetTag("Module", "cassandra")
+		span.SetTag("Interface", "sessionRetry")
+		impulseCtx.Span = span
+	} else {
+		span = opentracing.StartSpan("Query", opentracing.ChildOf(impulseCtx.Span.Context()))
+		defer span.Finish()
+		span.SetTag("Module", "cassandra")
+		span.SetTag("Interface", "sessionRetry")
+		impulseCtx.Span = span
+	}
+	ctx = context.WithValue(ctx, impulse_ctx.ImpulseCtxKey, impulseCtx)
 
-	log.Debug("running SessionRetry Query() method")
+	log.Debug(impulseCtx, "running SessionRetry Query() method")
 
 	return queryRetry{goCqlQuery: s.goCqlSession.Query(stmt, values...)}
 }
 
 // Close wrapper to be able to run goCql method
-func (s sessionRetry) Close(parentSpan opentracing.Span) {
-	span := opentracing.StartSpan("Close", opentracing.ChildOf(parentSpan.Context()))
-	defer span.Finish()
-	span.SetTag("Module", "cassandra")
-	span.SetTag("Interface", "sessionRetry")
+func (s sessionRetry) Close(ctx context.Context) {
+	var span opentracing.Span
+	impulseCtx, ok := ctx.Value(impulse_ctx.ImpulseCtxKey).(impulse_ctx.ImpulseCtx)
+	if !ok {
+		log.Warnf(impulseCtx, "ImpulseCtx isn't correct type")
+		span = opentracing.StartSpan("Close")
+		defer span.Finish()
+		span.SetTag("Module", "cassandra")
+		span.SetTag("Interface", "sessionRetry")
+		impulseCtx.Span = span
+	} else {
+		span = opentracing.StartSpan("Close", opentracing.ChildOf(impulseCtx.Span.Context()))
+		defer span.Finish()
+		span.SetTag("Module", "cassandra")
+		span.SetTag("Interface", "sessionRetry")
+		impulseCtx.Span = span
+	}
+	ctx = context.WithValue(ctx, impulse_ctx.ImpulseCtxKey, impulseCtx)
 
-	log.Debug("running SessionRetry Close() method")
+	log.Debug(impulseCtx, "running SessionRetry Close() method")
 
 	s.goCqlSession.Close()
 }
