4f03b611eb26e2eae031eb456a3d2b4c814905ca
Aristotle <11413392+tuxranger@users.noreply.github.com>
2022-08-30 17:02:50 -0400

    motiv-labs/impulse#386 started adding logic to timestamp partition functions

--- partitionTimestamp.go
+++ partitionTimestamp.go
@@ -2,6 +2,10 @@ package cassandra
 
 import (
 	"context"
+	"fmt"
+	impulse_ctx "github.com/motiv-labs/impulse-ctx"
+	log "github.com/motiv-labs/logwrapper"
+	"strings"
 	"time"
 )
 
@@ -10,13 +14,13 @@ type Timestamp interface {
 
 type timestamp struct {
 	sessionRetry sessionRetry
-	unixTime     int64
+	duration     time.Duration
 }
 
-func NewTimestamp(s sessionRetry, unixTime int64) Timestamp {
+func NewTimestamp(s sessionRetry, duration time.Duration) Timestamp {
 	return timestamp{
 		sessionRetry: s,
-		unixTime:     unixTime,
+		duration:     duration,
 	}
 }
 
@@ -27,8 +31,20 @@ Params:
 	unixTime: the unix time to use
 */
 func (t timestamp) CreatePartitionTimestampValue() int64 {
-	// todo create the partiion value based on the timestamp and unix value
-	return 0
+	// todo create the partition value based on the timestamp and unix value
+	var unixTime int64
+	switch t.duration {
+	case time.Millisecond:
+		unixTime = time.Now().UnixMilli()
+	case time.Microsecond:
+		unixTime = time.Now().UnixMicro()
+	case time.Nanosecond:
+		unixTime = time.Now().UnixNano()
+	default: // default to seconds
+		unixTime = time.Now().Unix()
+	}
+
+	return unixTime
 }
 
 /*
@@ -37,21 +53,85 @@ Params:
 	ctx: context object with ImpulseCtx struct for logging and query functions
 	table: the table to query
 	where: where clause and additional options to pass the query
+	timeRangeColumn: the name of the column to use a time range on
 	start: start time to query by
 	end: end time to query by
 	limit: the number of records to look for and return
 */
-func (t timestamp) PartitionTimestampQuery(ctx context.Context, table, where string, start, end time.Time, limit int) []interface{} {
+func (t timestamp) PartitionTimestampQuery(ctx context.Context, table, where, timeRangeColumn string, start, end time.Time, limit int) []interface{} {
+	impulseCtx, ok := ctx.Value(impulse_ctx.ImpulseCtxKey).(impulse_ctx.ImpulseCtx)
+	if !ok {
+		log.Warnf(impulseCtx, "ImpulseCtx isn't correct type")
+	}
 	// todo build statement
+	query := t.buildCassQuery(table, where, timeRangeColumn, start, end, limit)
 	// todo perform query
 	// todo repeat query to next partition if limit is not met
 	return nil
 }
 
 /*
-buildWhereClause will build the where clause for a partition timestamp query
+buildCassQuery will build the cassandra statement for a partition timestamp query
+Params:
+	table: the table to query
+	where: where clause and additional options to pass the query
+	timeRangeColumn: the name of the column to use a time range on
+	start: start time to query by
+	end: end time to query by
+	limit: the number of records to look for and return
 */
-func (t timestamp) buildCassStmt(table, where string, start, end time.Time) string {
+func (t timestamp) buildCassQuery(table, where, timeRangeColumn string, start, end time.Time, limit int) string {
 	// todo build cassandra statement to be used in timestamp query
-	return ""
+	// build initial select clause
+	selectClause := fmt.Sprintf("SELECT * FROM %s", table)
+
+	// build where clause
+	// build in section
+	var startTime int64
+	var endTime int64
+	switch t.duration {
+	case time.Millisecond:
+		startTime = start.UnixMilli()
+		endTime = end.UnixMilli()
+	case time.Microsecond:
+		startTime = start.UnixMicro()
+		endTime = end.UnixMilli()
+	case time.Nanosecond:
+		startTime = start.UnixNano()
+		endTime = end.UnixMilli()
+	default: // default to seconds
+		startTime = start.Unix()
+		endTime = end.UnixMilli()
+	}
+
+	// open in clause
+	inClause := "IN ("
+
+	// todo understand and fix this int64 conversion logic based on https://github.com/hailocab/gocassa/blob/master/timeseries_table.go#L51
+	for i := startTime; ; i += int64(t.duration/t.duration) * 1000 {
+		if i >= endTime*1000 {
+			break
+		}
+		// todo fix this append
+		if i+int64(t.duration/t.duration)*1000 >= endTime*1000 {
+			inClause = fmt.Sprintf("%s")
+		} else {
+			inClause = fmt.Sprintf("%s")
+		}
+	}
+	// close in clause
+	inClause = fmt.Sprintf("%s)", inClause)
+
+	whereClause := fmt.Sprintf("WHERE %s %s", timeRangeColumn, inClause)
+	if where != "" {
+		whereClause = fmt.Sprintf("%s AND %s", whereClause, where)
+	}
+
+	// build limit clause
+	limitClause := fmt.Sprintf("LIMIT %d", limit)
+
+	// combine all clauses to create the query
+	query := strings.Join([]string{selectClause, whereClause, limitClause}, " ")
+
+	return query
 }
