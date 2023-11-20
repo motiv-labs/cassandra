e7564224c33b5f85d73cfcf33b53748a486f00c7
Aristotle Tesser <11413392+tuxranger@users.noreply.github.com>
2023-11-20 10:09:40 -0500

    motiv-labs/impulse#521 create snapshot connectors (#15)
    * implemented mapscanandclose func
    
    * updated mapscanandclose func
    
    * testing pointer map
    
    * removed mapscanandclose from interface

--- queryretry.go
+++ queryretry.go
@@ -396,11 +396,12 @@ func (i iterRetry) ScanAndClose(ctx context.Context, handle func() bool, dest ..
 }
 
 // todo test this function
+// todo solve m not updating for the outer function who calls this one.
 // MapScanAndClose is a wrapper to retry around the gocql MapScan() and Close().
 // We have a retry approach in place + incremental approach used. For example:
 // First time it will wait 1 second, second time 2 seconds, ... It will depend on the values for retries
 // and seconds to wait.
-func (i iterRetry) MapScanAndClose(m map[string]interface{}, handle func(), ctx context.Context) error {
+func (i iterRetry) MapScanAndClose(m *map[string]interface{}, handle func() bool, ctx context.Context) error {
 	impulseCtx, ok := ctx.Value(impulse_ctx.ImpulseCtxKey).(impulse_ctx.ImpulseCtx)
 	if !ok {
 		log.Error(impulseCtx, "ImpulseCtx isn't correct type")
@@ -417,8 +418,12 @@ func (i iterRetry) MapScanAndClose(m map[string]interface{}, handle func(), ctx 
 
 		// Scan consumes the next row of the iterator and copies the columns of the
 		// current row into the values pointed at by dest.
-		for i.MapScan(m, ctx) {
-			handle()
+		for i.MapScan(*m, ctx) {
+			if !handle() {
+				break
+			}
+			// need to reset m otherwise MapScan errors
+			m = &map[string]interface{}{}
 		}
 
 		// we will try to run the method several times until attempts is met
